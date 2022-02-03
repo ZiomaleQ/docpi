24a6da3a11e23d8ad206abfdf8a550c9a51b3212
ZiomaleQ <r.partyka30@gmail.com>
2022-02-03 18:35:15 +0100

    Fix docs gen

--- mod.ts
+++ mod.ts
@@ -1,11 +1,23 @@
 import { serve } from 'https://deno.land/std@0.124.0/http/server.ts'
 import { doc } from "https://deno.land/x/deno_doc@v0.26.0/mod.ts";
 
-serve((req) => {
+serve(async (req) => {
 
 	const entrypoint = new URL(req.url).searchParams.get('entrypoint')
 
-	return new Response(JSON.stringify(doc(entrypoint)), {
-		headers: { "content-type": "application/json" }
-	})
+	try {
+
+		const docs = await doc(entrypoint ?? "https://raw.githubusercontent.com/denoland/deno_doc/main/mod.ts")
+
+		return new Response(JSON.stringify(docs), {
+			headers: { "content-type": "application/json" }
+		})
+	} catch (_err) {
+		return new Response("{\"error\":\"Couldn't generate the docs\"}",
+		{
+			status: 400,
+			headers: { "content-type": "application/json" }
+		})
+	}
+	
 })
